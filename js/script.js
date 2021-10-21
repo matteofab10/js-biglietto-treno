c8bcfec0393c06a536f40e2e8452a5bb748d10bd
matteofab10 <matteofabio10@gmail.com>
2021-10-21 17:04:18 +0200

    fine esercizio senza bonus

--- js/script.js
+++ js/script.js
@@ -2,12 +2,34 @@
 
 1.chiedi all'utente quanti km vuole percorrere
 2.chiedi all'utente la sua età
-3.applicare lo sconto del 20% ai minorenni
-4.applicare lo sconto del 40% agli over 65
-5.output del prezzo finale con massimo due decimali
+3.prezzo del biglietto definito in base ai km (0.21 € al km)
+4.applicare lo sconto del 20% ai minorenni
+5.applicare lo sconto del 40% agli over 65
+6.output del prezzo finale con massimo due decimali
 
 */
 
 const kmToTrip = parseInt(prompt ("Quanti chilometri vorresti percorrere?"));
 const userAge = parseInt(prompt ("Quanti anni hai?"));
-const ticketPrice = kmToTrip * 0.21;
+let ticketPrice = kmToTrip * 0.21;
+let finalPrice;
+
+if (userAge <= 18){
+  ticketPrice = ticketPrice * 80 / 100;
+} else if (userAge >= 65){
+  ticketPrice = ticketPrice * 60 / 100;
+} else {
+  console.log("Nessuno sconto");
+}
+
+console.log(ticketPrice);
+
+ticketPrice = ticketPrice.toFixed(2);
+
+document.getElementById("output").innerHTML = 
+`
+  Chilometri: ${kmToTrip} <br>
+  Età viaggiatore: ${userAge} <br>
+  Costo biglietto: ${kmToTrip} * 0.21 = ${ticketPrice}€ <br>
+
+`;
